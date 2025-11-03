c6df2f720eeea269858f9df79921df9096abae0c
Richard Körner <grasbock@gmail.com>
2025-11-03 22:47:05 +0100

    do some basic ui setup similar to what I want

--- src/app.rs
+++ src/app.rs
@@ -1,25 +1,32 @@
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
+pub struct App {
     // Example stuff:
     label: String,
 
     #[serde(skip)] // This how you opt-out of serialization of a field
     value: f32,
+
+    objects: Vec<(f64, f64, egui::Color32, bool)>,
 }
 
-impl Default for TemplateApp {
+impl Default for App {
     fn default() -> Self {
         Self {
             // Example stuff:
             label: "Hello World!".to_owned(),
             value: 2.7,
+            objects: vec![
+                (10.0, 0.0, egui::Color32::GREEN, true),
+                (30.0, 20.0, egui::Color32::BLUE, false),
+                (40.0, -10.0, egui::Color32::RED, false),
+            ],
         }
     }
 }
 
-impl TemplateApp {
+impl App {
     /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customize the look and feel of egui using
@@ -35,7 +42,7 @@ impl TemplateApp {
     }
 }
 
-impl eframe::App for TemplateApp {
+impl eframe::App for App {
     /// Called by the framework to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
@@ -43,46 +50,41 @@ impl eframe::App for TemplateApp {
 
     /// Called each time the UI needs repainting, which may be many times per second.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
-
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-
-            egui::MenuBar::new().ui(ui, |ui| {
-                // NOTE: no File->Quit on web pages!
-                let is_web = cfg!(target_arch = "wasm32");
-                if !is_web {
-                    ui.menu_button("File", |ui| {
-                        if ui.button("Quit").clicked() {
-                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
-                        }
-                    });
-                    ui.add_space(16.0);
-                }
-
-                egui::widgets::global_theme_preference_buttons(ui);
-            });
-        });
-
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
+            ui.heading("FMCW Radar demo 0");
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
-            });
-
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
+            for (i, obj) in self.objects.iter_mut().enumerate() {
+                ui.horizontal(|ui| {
+                    ui.label(format!("Object {}", i + 1));
+                    ui.add(egui::Checkbox::new(&mut obj.3, ""));
+                    ui.add(egui::Slider::new(&mut obj.0, 0.0..=100.0).text("Range"));
+                    ui.add(egui::Slider::new(&mut obj.1, -20.0..=20.0).text("Velocity"));
+                });
             }
 
             ui.separator();
 
+            egui_plot::Plot::new("my_plot")
+                .height(100.0)
+                .auto_bounds(false)
+                .default_x_bounds(0.0, 100.0)
+                .default_y_bounds(-20.0, 20.0)
+                .show(ui, |plot_ui| {
+                    for (i, &(x, y, c, e)) in self.objects.iter().enumerate() {
+                        if !e {
+                            continue;
+                        }
+                        // Draw a sphere for each object as a circle on the plot
+                        let sphere = egui_plot::Points::new(format!("sphere_{i}"), vec![[x, y]])
+                            .radius(8.0)
+                            .color(c);
+                        plot_ui.points(sphere);
+                    }
+                });
+
             ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/main/",
+                "https://github.com/GRASBOCK/fmcw-radar_demo-0/blob/main/",
                 "Source code."
             ));
 
