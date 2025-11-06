ff77e7b0d41b5d98d9122431a18dd4a3da2c66dc
GRASBOCK <31473223+GRASBOCK@users.noreply.github.com>
2025-11-06 23:39:25 +0000

    Deploying to gh-pages from @ GRASBOCK/fmcw-radar_demo-0@0fdf6817cc884e2b024792d40a92016690d0999e 🚀

---
+++ sw.js
@@ -0,0 +1,25 @@
+var cacheName = 'egui-template-pwa';
+var filesToCache = [
+  './',
+  './index.html',
+  './fmcw_radar_demo0.js',
+  './fmcw_radar_demo0_bg.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
